04409d119982f808be5806bf26b4e8ed2c18f759
otaviob <otaviob22@gmail.com>
2025-06-05 12:17:10 +1000

    Configure initial scene with UICollectionViewController

---
+++ ios-pokedex-app-swift/Controllers/PokedexCollectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-pokedex-app-swift
+//
+//  Created by Otavio Brito on 5/6/2025.
+//
+
+import UIKit
+
+class PokedexCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .systemRed
+        
+    }
+
+
+}
+
