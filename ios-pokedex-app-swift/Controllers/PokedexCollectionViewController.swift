0e27bfebe68b77b8a27e32188fcfb24e9accbe8e
otaviob <otaviob22@gmail.com>
2025-06-24 23:51:20 +1000

    Init Visual Effect View

--- ios-pokedex-app-swift/Controllers/PokedexCollectionViewController.swift
+++ ios-pokedex-app-swift/Controllers/PokedexCollectionViewController.swift
@@ -21,6 +21,12 @@ class PokedexCollectionViewController: UICollectionViewController {
         return view
     }()
     
+    let visualEffectView: UIVisualEffectView = {
+        let blurEffect = UIBlurEffect(style: .dark)
+        let view = UIVisualEffectView(effect: blurEffect)
+        return view
+    }()
+    
     // MARK: - View Lifecycle
 
     override func viewDidLoad() {
@@ -65,7 +71,9 @@ class PokedexCollectionViewController: UICollectionViewController {
         
         collectionView.register(PokedexCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         
-        
+        view.addSubview(visualEffectView)
+        visualEffectView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        visualEffectView.alpha = 0
         
     }
 }
@@ -107,6 +115,15 @@ extension PokedexCollectionViewController: PokedexCollectionViewCellDelegate {
         infoView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: view.frame.width - 64, height: 350)
         infoView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         infoView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -44).isActive = true
+        
+        infoView.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        infoView.alpha = 0
+        
+        UIView.animate(withDuration: 0.5) {
+            self.visualEffectView.alpha = 1
+            self.infoView.alpha = 1
+            self.infoView.transform = .identity
+        }
     }
     
     
