2e19799f4ebc5110502b7395cae93fabd5da4d44
otaviob <otaviob22@gmail.com>
2025-08-24 23:04:10 +1000

    Description Name PokedexInfoViewController

---
+++ ios-pokedex-app-swift/Controllers/PokedexInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonInfoController.swift
+//  ios-pokedex-app-swift
+//
+//  Created by Otavio Brito on 24/8/2025.
+//
+
+import UIKit
+
+class PokedexInfoController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    
+    
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewComponents()
+    }
+    
+    // MARK: - Helper Functions
+    func configureViewComponents() {
+        view.backgroundColor = .white
+    }
+}
--- ios-pokedex-app-swift/Controllers/PokemonInfoController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  PokemonInfoController.swift
-//  ios-pokedex-app-swift
-//
-//  Created by Otavio Brito on 24/8/2025.
-//
-
-import Foundation
